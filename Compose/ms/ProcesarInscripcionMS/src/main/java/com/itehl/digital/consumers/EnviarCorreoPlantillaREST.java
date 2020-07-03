904429983abdfc3cfc090c831f97c5f018e26a2b
repo-itehl <jorge.heredia@itehl.com>
2020-07-02 22:02:06 -0500

    Update EnviarCorreoPlantillaREST.java

--- Compose/ms/ProcesarInscripcionMS/src/main/java/com/itehl/digital/consumers/EnviarCorreoPlantillaREST.java
+++ Compose/ms/ProcesarInscripcionMS/src/main/java/com/itehl/digital/consumers/EnviarCorreoPlantillaREST.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.HttpServerErrorException;
 
 public class EnviarCorreoPlantillaREST {
 
