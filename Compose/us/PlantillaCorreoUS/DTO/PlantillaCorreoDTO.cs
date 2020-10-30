c4a066ad900604f170d8af44bbf94c2d9892ec70
Jorge Heredia <jorge.heredia@itehl.com>
2020-10-30 10:01:47 -0500

    Adaptación de artefactos para K8S en GKE.

--- Compose/us/PlantillaCorreoUS/DTO/PlantillaCorreoDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PlantillaCorreoUS.DTO
-{
-    public class PlantillaCorreoDTO
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Html { get; set; }
-    }
-}
